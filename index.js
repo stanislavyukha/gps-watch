35b3f19b06993beb919614abcdd6e410f274a4e7
Stas Yukha <valhalla245@gmail.com>
2021-07-11 16:57:16 +0300

    removed controls

--- index.js
+++ index.js
@@ -3,26 +3,6 @@ let markers = [];
 const progressContainer = document.querySelector('.progress');
 const progressBar = document.querySelector('.progress-bar');
 const mapContainer = document.querySelector('.map');
-const zoomIn = document.querySelector('#zoomin');
-const zoomOut = document.querySelector('#zoomout');
-
-
-function HomeControl(controlDiv, map) {
-    google.maps.event.addDomListener(zoomout, 'click', function () {
-      var currentZoomLevel = map.getZoom();
-      if (currentZoomLevel != 0) {
-        map.setZoom(currentZoomLevel - 1);
-      }
-    });
-
-    google.maps.event.addDomListener(zoomin, 'click', function () {
-      var currentZoomLevel = map.getZoom();
-      if (currentZoomLevel != 21) {
-        map.setZoom(currentZoomLevel + 1);
-      }
-    });
-  }
-
 
 
 function initMap() {
@@ -33,18 +13,11 @@ function initMap() {
             lng: position.coords.longitude
         }
         const map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 15,
+            zoom: 16,
             center: coordinates,
             disableDefaultUI: true,
             zoomControl: true,
-            mapTypeControl: true,
           });
-          var homeControlDiv = document.createElement('div');
-          var homeControl = new HomeControl(homeControlDiv, map);
-          homeControlDiv.index = 1;
-          map.controls[google.maps.ControlPosition.TOP_LEFT].push(homeControlDiv);
-
-
           const marker = new google.maps.Marker({
             position: coordinates,
             title:"You are here"
@@ -55,9 +28,7 @@ function initMap() {
         setTimeout(() => {
           progressContainer.classList.add('hide');
           mapContainer.classList.remove('hide');
-          zoomIn.classList.remove('hide');
-          zoomOut.classList.remove('hide');
-        //   mapContainer.style.position = 'static';
+         
         }, 1000);
     }, () => {
         console.log('no geo access');
